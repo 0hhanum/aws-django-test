46f160cdbbd08a5a86d3cebc6e25041fd6afaed4
hanum <rntls123@naver.com>
2021-05-10 03:08:00 +0900

    6

--- config/urls.py
+++ config/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
+from django.conf.urls import url
 
 
 urlpatterns = [
@@ -18,4 +20,13 @@ urlpatterns = [
 
 
 # if settings.DEBUG:
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpattern += url(
+    r"^media/(?P<path>.\*)$",
+    serve,
+    {
+        "document_root": settings.MEDIA_ROOT,
+    },
+)
+urlpattern += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
